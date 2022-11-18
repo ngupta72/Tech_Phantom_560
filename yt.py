a5439bf7ad973cd6d9b114580bda912222754882
Neha Gupta <ngupta72@usc.edu>
2022-11-18 10:24:58 -0800

    Updated app file

--- yt.py
+++ yt.py
@@ -67,7 +67,7 @@ def add_bg_from_local(image_file):
 	)
 
 with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+	st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Load Our Dataset
 # def load_data(data):
@@ -253,20 +253,24 @@ def recommend_courses(course_id, n_courses, df_courses, df_norm):
 	df_sorted['cosine_similarity'] = cos_sim
 	df_sorted=df_sorted.sort_values('cosine_similarity', ascending=False).reset_index(drop=True)
 	
-	return title, y_pred, y_true, df_sorted.iloc[1:n_courses][['id','published_title', 'cosine_similarity', 'primary_subcategory']]
+	return title, y_pred, y_true, df_sorted.iloc[1:n_courses][['id','published_title', 'cosine_similarity', 'primary_subcategory', 'price', 'objectives']]
 
-def recommend_courses_search(search_text, n_courses, df_courses, df_norm):
-	if df_courses['published_title'].str.contains(search_text).sum() > 1:
-		print("Lots of it")
-	elif df_courses['published_title'].str.contains(search_text).sum() == 0:
-		return 'No course found'
-	elif df_courses['published_title'].str.contains(search_text).sum() == 1:
+@st.cache
+def recommend_courses_search(search_text, n_courses, df_courses, df_norm, df):
+
+	flag_sum = df['title'].str.lower().str.contains(str(search_text).lower()).sum()
+	if flag_sum > 1:
+		#print("Lots of it")
+		return flag_sum, False, False, False, df[df['title'].str.lower().str.contains(str(search_text).lower())]
+
+	elif flag_sum == 1:
 		#print(df_courses['published_title'].str.contains(search_text).sum())
-		print(df_courses[df_courses['published_title'].str.contains(search_text)]['published_title'])
+		#print(df_courses[df_courses['published_title'].str.contains(search_text)]['published_title'])
+		y_pred = df_courses['primary_subcategory']
 		n_courses=n_courses+1
-		id_=df_courses[df_courses['published_title'].str.contains(search_text)].index.values
-		title=df_courses[df_courses['published_title'].str.contains(search_text)]['published_title']
-		y_true = [df_courses.loc[df_courses['published_title'].str.contains(search_text)]['primary_subcategory'].item()] * (n_courses-1)
+		id_=df_courses[df_courses['id'].isin(df[df['title'].str.lower().str.contains(str(search_text).lower())]['id'])].index.values
+		title=df_courses[df_courses['id'].isin(df[df['title'].str.lower().str.contains(str(search_text).lower())]['id'])]['published_title']
+		y_true = [df_courses.loc[df_courses['id'].isin(df[df['title'].str.lower().str.contains(str(search_text).lower())]['id'])]['primary_subcategory'].item()] * (n_courses-1)
 		X = df_norm.values
 		Y = df_norm.loc[id_].values.reshape(1, -1)
 		cos_sim = cosine_similarity(X, Y)
@@ -274,7 +278,11 @@ def recommend_courses_search(search_text, n_courses, df_courses, df_norm):
 		df_sorted['cosine_similarity'] = cos_sim
 		df_sorted=df_sorted.sort_values('cosine_similarity', ascending=False).reset_index(drop=True)
 
-	return title, y_true, df_sorted.iloc[1:n_courses][['id','published_title', 'cosine_similarity', 'primary_subcategory']]
+		return flag_sum, title, y_pred, y_true, df_sorted.iloc[1:n_courses][['id','published_title', 'cosine_similarity', 'primary_subcategory', 'price', 'objectives']]
+
+	else:
+		return flag_sum, False, False, False, False
+
 
 def recommend_for_user(user_name, n_courses, df_reviews, df_courses, df_norm):
 	list_courses=df_reviews[df_reviews['user_name']==user_name]['course_id'].values
@@ -302,76 +310,6 @@ def recommend_for_user(user_name, n_courses, df_reviews, df_courses, df_norm):
 		print('The following courses are recommended after all taken courses:')
 		print(df_temp.iloc[1:n_courses][['published_title', 'avg_cos_sim']])
 
-# def build_recommender_system(df_courses, df_reviews):
-# 	import nltk
-# 	nltk.download('punkt')
-# 	nltk.download('stopwords')
-
-# 	objectives_text=df_courses['objectives'].apply(combine_list)
-# 	vocab_frame_orig=vocab_stem(objectives_text)
-# 	#drop duplicates from the dataframe with stemmed words
-# 	vocab_frame=drop_words(vocab_frame_orig)
-# 	StopWords=set(stopwords.words('english')+list(punctuation)+["’", "n't", "'s", "--", "-", "...", "``", "''", "“", "039"])
-# 	#use TfidfVectorizer 
-# 	vectorizer= TfidfVectorizer(stop_words=StopWords, tokenizer=tokenize, max_features=1000, max_df=0.8)
-# 	X=vectorizer.fit_transform(objectives_text)
-# 	word_features = vectorizer.get_feature_names()
-# 	vocab_frame_descr=vocab_stem(df_courses['description_text'])
-# 	vocab_frame_descr=drop_words(vocab_frame_descr)
-# 	StopWords=set(stopwords.words('english')+list(punctuation)+["’", "n't", "'s", "--", "-", "...", "``", "''", "“", "039"])
-# 	#use TfidfVectorizer
-# 	vectorizer_descr= TfidfVectorizer(stop_words=StopWords, tokenizer=tokenize, max_features=1000, max_df=0.8)
-# 	X_descr=vectorizer_descr.fit_transform(df_courses['description_text'])
-# 	word_features_descr = vectorizer_descr.get_feature_names()
-
-# 	kmeans = KMeans(n_clusters = 7, n_init = 10, n_jobs = -1, random_state=1234)
-# 	kmeans.fit(X)
-# 	common_words=get_common_words(kmeans, 10)
-# 	squarify_words(common_words, word_features, vocab_frame)
-# 	df_courses['cluster']=kmeans.labels_
-# 	heatmap_categories_cluster('cluster', df_courses, 'Reds')
-# 	plot_common_words(kmeans, 5, word_features, vocab_frame, df_courses, 'cluster')
-
-# 	kmeans_descr = KMeans(n_clusters = 8, n_init = 10, n_jobs = -1, random_state=123456)
-# 	kmeans_descr.fit(X_descr)
-# 	common_words=get_common_words(kmeans_descr, 10)
-# 	squarify_words(common_words, word_features_descr, vocab_frame_descr)
-# 	df_courses['cluster_descr']=kmeans_descr.labels_
-# 	heatmap_categories_cluster('cluster_descr', df_courses, 'Reds')
-# 	plot_common_words(kmeans_descr, 5, word_features_descr, vocab_frame_descr, df_courses, 'cluster_descr')
-
-# 	rel_cols=['avg_rating',  'has_certificate',  'instructional_level', 'num_lectures','num_quizzes',
-#           'num_practice_tests','is_practice_test_course', 'num_article_assets', 'num_curriculum_items',
-#           'num_subscribers','num_reviews',  'price', 'primary_subcategory','cluster_descr']
-# 	df_rel=df_courses[rel_cols]
-	
-# 	df_rel['has_certificate']=df_rel['has_certificate'].astype(int)
-# 	df_rel['cluster_descr']=df_rel['cluster_descr'].astype(str)
-# 	dummies=pd.get_dummies(df_rel[['primary_subcategory', 'instructional_level','cluster_descr']], prefix=['subcat', 'level', 'cluster'])
-# 	df_rel.drop(columns=['primary_subcategory', 'instructional_level', 'cluster_descr'], inplace=True)
-# 	df_rel=pd.concat([df_rel,dummies], axis=1)
-# 	pd.set_option('display.max_rows', None)
-# 	df_norm=normalize_features(df_rel)
-# 	#nr_user=df_reviews['user_name'].value_counts()
-# 	#unique, counts = np.unique(nr_user, return_counts=True)
-
-# 	return df_courses, df_norm
-
-# def recommend_courses(course_id, n_courses, df, df_norm):
-#     n_courses=n_courses+1
-#     id_=df[df['id']==course_id].index.values
-#     title=df[df['id']==course_id]['published_title']
-#     y_true = [df.loc[df['id']==course_id]['primary_subcategory'].item()] * (n_courses-1)
-#     X = df_norm.values
-#     Y = df_norm.loc[id_].values.reshape(1, -1)
-#     cos_sim = cosine_similarity(X, Y)
-#     df_sorted=df.copy()
-#     df_sorted['cosine_similarity'] = cos_sim
-#     df_sorted=df_sorted.sort_values('cosine_similarity', ascending=False).reset_index(drop=True)
-
-#     return title, y_true, df_sorted.iloc[1:n_courses][['id','published_title', 'cosine_similarity', 'primary_subcategory']]
-
-
 RESULT_TEMP = """
 <div style="width:90%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;border-bottom-right-radius: 60px;
 box-shadow:0 0 15px 5px #ccc; background-color: #a8f0c6;
@@ -391,24 +329,37 @@ def search_term_if_not_found(term,df):
 	return result_df
 
 def initialize_course_widget(no):
-    # with st.expander(cfg["title"]):
-    #     st.markdown(cfg["description"])
+	course_cols = st.columns(no)
+	for c in course_cols:
+		with c:
+			st.empty()
 
-    course_cols = st.columns(no)
-    for c in course_cols:
-        with c:
-            st.empty()
+	return course_cols
 
-    return course_cols
+def fetch_course_details(result, df):
+	title_list = []
+	image = []
+	url = []
+	inst = []
+	ratings = []
+	for i in range(len(result)):
+		image.append(df[df['id']==result['id'].values[i]]['image_200_H'].values[0])
+		title_list.append(df[df['id']==result['id'].values[i]]['title'].values[0])
+		url.append('https://www.udemy.com' + df[df['id']==result['id'].values[i]]['url'].values[0])
+		inst.append(df[df['id']==result['id'].values[i]]['instructional_level_simple'].values[0])
+		ratings.append(df[df['id']==result['id'].values[i]]['rating'].values[0])
+
+	return title_list, image, url, inst, ratings
 
 def main():
 
 	st.title("Analytical Study on Course Recommendation System for Online Courses")
 
-	menu = ["Home", "EDA", "Recommend","About"]
+	menu = ["Home", "Recommend by CourseID", "Recommend by Title", "About"]
 	choice = st.sidebar.selectbox("Menu",menu)
 
 	df = pd.read_csv("df_courses3.csv")
+	df = df.drop_duplicates(subset='id', keep="first")
 
 	df_courses=pd.read_csv('df_courses_recomm.csv', index_col=0)
 	df_reviews=pd.read_csv('df_reviews_cleaned.csv', index_col=0)
@@ -421,43 +372,43 @@ def main():
 
 		st.dataframe(df.head(10))
 	
-	if choice =="EDA":
-		st.subheader('Udemy Dataset')
-		st.markdown('Data about Udemy was also gathered from Kaggle, which concentrates on four different topics pertaining to business finance, graphic design, musical instruments, and web design offered on the platform. Course Difficulty and the number of reviews for the course are the key elements in this analysis, which will aid in the development of the recommendation model.')
+	# if choice =="EDA":
+	# 	st.subheader('Udemy Dataset')
+	# 	st.markdown('Data about Udemy was also gathered from Kaggle, which concentrates on four different topics pertaining to business finance, graphic design, musical instruments, and web design offered on the platform. Course Difficulty and the number of reviews for the course are the key elements in this analysis, which will aid in the development of the recommendation model.')
 
-		image = Image.open('Udemy_Summary1.png')
-		st.image(image, caption='Summary and Statistics for Udemy Dataset')
+	# 	image = Image.open('Udemy_Summary1.png')
+	# 	st.image(image, caption='Summary and Statistics for Udemy Dataset')
 
 
-		image = Image.open('Udemy6.png')
-		st.image(image, caption='Course Ratiings')
-		st.markdown('There are around 900 courses with no reviews/ratings, but most of the ratings are between rating 4 and 4.5.')
+	# 	image = Image.open('Udemy6.png')
+	# 	st.image(image, caption='Course Ratiings')
+	# 	st.markdown('There are around 900 courses with no reviews/ratings, but most of the ratings are between rating 4 and 4.5.')
 
-		image = Image.open('Udemy1.png')
-		st.image(image, caption='Price range of the courses')
-		st.markdown('The bar chart shows the price range of the courses. The price ranges between 0 and 199 EUR. Most courses cost eiter 19.99 or 199.99 $.')
+	# 	image = Image.open('Udemy1.png')
+	# 	st.image(image, caption='Price range of the courses')
+	# 	st.markdown('The bar chart shows the price range of the courses. The price ranges between 0 and 199 EUR. Most courses cost eiter 19.99 or 199.99 $.')
 
-		image = Image.open('Udemy2.png')
-		st.image(image, caption='Courses with high number of subscribers')
-		st.markdown('The plot shows courses that are most visited. The most popular course is Java with more than 300.000 subscribers. Courses with the top 10 most subscribers can be seen')
+	# 	image = Image.open('Udemy2.png')
+	# 	st.image(image, caption='Courses with high number of subscribers')
+	# 	st.markdown('The plot shows courses that are most visited. The most popular course is Java with more than 300.000 subscribers. Courses with the top 10 most subscribers can be seen')
 
-		image = Image.open('Udemy3.png')
-		st.image(image, caption='Number of reviews per course')
-		st.markdown('Number of reviews per course: most courses have very few reviews. The limit of the available reviews from the API is 10.000, hence there are couple courses around 10.000 ')
+	# 	image = Image.open('Udemy3.png')
+	# 	st.image(image, caption='Number of reviews per course')
+	# 	st.markdown('Number of reviews per course: most courses have very few reviews. The limit of the available reviews from the API is 10.000, hence there are couple courses around 10.000 ')
 
-		image = Image.open('Udemy4.png')
-		st.image(image, caption='Number of reviews per user')
-		st.markdown('The first 20 values of the most common number of reviews per user most users were plotted. More than 600000 users have only 1 review')
+	# 	image = Image.open('Udemy4.png')
+	# 	st.image(image, caption='Number of reviews per user')
+	# 	st.markdown('The first 20 values of the most common number of reviews per user most users were plotted. More than 600000 users have only 1 review')
 
-		image = Image.open('Udemy5.png')
-		st.image(image, caption='Usernames with the most reviews')
-		st.markdown('Plotted the usernames with the most reviews - Username is not unique')
+	# 	image = Image.open('Udemy5.png')
+	# 	st.image(image, caption='Usernames with the most reviews')
+	# 	st.markdown('Plotted the usernames with the most reviews - Username is not unique')
 	
-	elif choice == "Recommend":
-		st.subheader("Recommend Courses")
+	elif choice == "Recommend by CourseID":
+		st.subheader("Recommend Courses by CourseID")
 		
 		search_course_id = st.number_input("Search by course id", min_value=0)
-		n_courses = st.number_input("Number",max_value=100)
+		n_courses = st.number_input("Number (upto 50)",max_value=50)
 		if st.button("Recommend"):
 			if search_course_id is not None:
 				try:
@@ -472,32 +423,23 @@ def main():
 					# title, y_true, result =recommend_courses(1259404, 10, df_courses, df_norm) #30% but the courses are relevant to the 
 					#title, y_pred, y_true, result = recommend_courses(2234122, 10, df_courses, df_norm) # 90%
 					title, y_pred, y_true, result = recommend_courses(search_course_id, n_courses, df_courses, df_norm)
+					st.write('Course: ', str(search_course_id), '-', title.values[0], '(', y_true[0], ')')
 					cols = initialize_course_widget(len(result))
-					titles = result['published_title']
+					ids = result['id']
 					categorys = result['primary_subcategory']
+					price = result['price']
+					titles, poster, url, inst, ratings = fetch_course_details(result, df)
 					#counter = 0
-					for c, t, cat in zip(cols, titles, categorys):
+					for c, d, t, cat, p, price, u, i, r in zip(cols, ids, titles, categorys, poster, price, url, inst, ratings):
 						with c:
-							st.markdown(f"<a style='display: block; text-align: center;' href='#'>{t}</a>", unsafe_allow_html=True)
-							st.write(cat)
-					#for i in range(len(result)):
-					# with col1:
-					# 	st.text(result['published_title'])
-					# 	st.image(posters[0])
-					# with col2:
-					# 	st.text(names[1])
-					# 	st.image(posters[1])
-					# with col3:
-					# 	st.text(names[2])
-					# 	st.image(posters[2])
-					# with col4:
-					# 	st.text(names[3])
-					# 	st.image(posters[3])
-					# with col5:
-					# 	st.text(names[4])
-					# 	st.image(posters[4])
-					st.write('Course: ', str(search_course_id), '-', title.values[0], '(', y_[0], ')')
-					st.dataframe(result)
+							st.image(p)
+							st.markdown(f"<a style='display: block; text-align: center;' href={u}>{t}</a>", unsafe_allow_html=True)
+							st.write("Course ID: ", d)
+							st.write("Category: ", cat)
+							# st.write(price)
+							st.write(i)
+							st.write("Rating: ", r)
+					
 				except:
 					result = "Not Found"
 					st.warning(result)
@@ -505,31 +447,36 @@ def main():
 					#result_df = search_term_if_not_found(search_course_id,df)
 					#st.dataframe(result_df)
 
-			# if search_text is not None:
-			# 	try:
-			# 		results = recommend_courses(search_text,df,n_courses)
-			# 		with st.beta_expander("Results as JSON"):
-			# 			results_json = results.to_dict('index')
-			# 			st.write(results_json)
+			
+	elif choice == "Recommend by Title":
+		st.subheader("Recommend Courses by Title")
 
-			# 		for row in results.iterrows():
-			# 			rec_title = row[1][0]
-			# 			rec_score = row[1][1]
-			# 			rec_url = row[1][2]
-			# 			rec_price = row[1][3]
-			# 			rec_num_sub = row[1][4]
+		options = st.multiselect('Search for a course title (select any one)', df["title"].unique())
+		#options = st.selectbox('Search for a course title', df["title"].unique())
+		n_courses = st.number_input("Number (upto 50)", min_value=1, max_value=50)
+		show_courses_recomm_btn = st.button("Recommend")
 
-			# 			stc.html(RESULT_TEMP.format(rec_title,rec_score,rec_url,rec_url,rec_num_sub),height=350)
-			# 	except:
-			# 		results= "Not Found"
-			# 		st.warning(results)
-			# 		st.info("Suggested Options include")
-			# 		result_df = search_term_if_not_found(search_text,df)
-			# 		st.dataframe(result_df)
-
-
-			# 	# How To Maximize Your Profits Options Trading
-
+		if show_courses_recomm_btn:
+			selected_title_id = df[df['title'] == options[0]]['id'].values[0]
+			print(selected_title_id)
+			title, y_pred, y_true, result = recommend_courses(selected_title_id, n_courses, df_courses, df_norm)
+			st.write('Course: ', selected_title_id, '-', title.values[0], '(', y_true[0], ')')
+			cols = initialize_course_widget(len(result))
+			ids = result['id']
+			categorys = result['primary_subcategory']
+			price = result['price']
+			titles, poster, url, inst, ratings = fetch_course_details(result, df)
+			#counter = 0
+			for c, d, t, cat, p, price, u, i, r in zip(cols, ids, titles, categorys, poster, price, url, inst, ratings):
+				with c:
+					st.image(p)
+					st.markdown(f"<a style='display: block; text-align: center;' href={u}>{t}</a>", unsafe_allow_html=True)
+					st.write("Course ID: ", d)
+					st.write("Category: ", cat)
+					# st.write(price)
+					st.write(i)
+					st.write("Rating: ", r)
+					
 
 	else:
 		st.subheader("About")
